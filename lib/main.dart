dcc24864bb7ebd644c52b1378772c2ad9fde50ca
ouril <ourilkreg@gmail.com>
2018-11-10 19:11:18 +0300

    star to explore!

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/ui/home.dart';
+
+void main() => runApp(
+
+    new MaterialApp(
+      home: Home()
+    )
+);
+
+
+
---
+++ lib/ui/home.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+// ignore: missing_identifier
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      title: Text("hello!!!"),
+      backgroundColor: Colors.amberAccent,
+      ),
+    );
+  }
+}
