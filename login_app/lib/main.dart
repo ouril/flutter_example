34816798444b8f85837959abdc3ce440e482ba61
ouril <ourilkreg@gmail.com>
2018-11-25 17:36:28 +0300

    added input fields in login app

--- login_app/lib/main.dart
+++ login_app/lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:login_app/ui/login.dart';
 void main() => runApp(
     MaterialApp(
-      title: 'Flutter Demo',
       home: LoginPage(),
     ));
 
--- login_app/lib/ui/login.dart
+++ login_app/lib/ui/login.dart
@@ -13,6 +13,10 @@ class LoginPage extends StatefulWidget {
 
 
 class LoginState extends State<LoginPage>{
+
+  final TextEditingController _userEditingController = new TextEditingController();
+  final TextEditingController _passEditingController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
@@ -23,7 +27,58 @@ class LoginState extends State<LoginPage>{
 
       ),
       backgroundColor: Colors.blueGrey,
-    );
+      body: new Container(
+        alignment: Alignment.topCenter,
+        child: new Column(
+          children: <Widget>[
+            new Image.asset(
+              'images/face.png',
+              width: 90.0,
+              height: 90.0,
+              color: Colors.greenAccent,
+            ),
+            new Container(
+              width: 420.0,
+              height: 280.0,
+              color: Colors.white,
+              alignment: Alignment.topCenter,
+              child: new Column(
+                children: <Widget>[
+                  new Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween ,
+                    children: <Widget>[
+                      Expanded(
+                      child: new Column(
+                        children: <Widget> [
+
+                       new TextField(
+                          controller: _userEditingController,
+                          decoration: new InputDecoration(
+                          hintText: 'Enter User name',
+                          icon: new Icon(Icons.person_add)
+                        ),
+                      ),
+                       new TextField(
+                          controller: _passEditingController,
+                          decoration: new InputDecoration(
+                          hintText: 'Enter User pass',
+                          icon: new Icon(Icons.terrain)
+                      ),
+                    )
+                          ]
+
+                      )),
+
+                ],
+              ),
+              ]
+            ),
+
+        ),
+      ]),
+
+
+    ));
   }
 
 }
